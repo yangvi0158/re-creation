9c346fcae65340bb13574c22b25801b31bd9f0bc
Hans Chiu <chiuhans111@gmail.com>
2019-12-15 17:41:48 +0800

    [VFX] 球球V1

--- src/components/vfx/keyvision/ball.vue
+++ src/components/vfx/keyvision/ball.vue
@@ -1,14 +1,43 @@
 <template>
-  <div class="vfx-keyvision_ball"></div>
+  <div class="vfx-keyvision-ball">
+    <div class="vfx-keyvision-ball_content">
+      <canvas ref="vfx-keyvision-ball_canvas" class="vfx-keyvision-ball_canvas"></canvas>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import ball from './ball.js'
+export default {
+  data () {
+    return {
+      handler: null
+    }
+  },
+  mounted () {
+    this.handler = ball.setup(this.$refs['vfx-keyvision-ball_canvas'])
+  },
+  destroyed () {
+    this.handler.destroy()
+  }
+}
 </script>
 
 <style scoped>
-.vfx-keyvision_ball {
+.vfx-keyvision-ball {
   position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+}
+
+.vfx-keyvision-ball_content {
+  width: 100%;
+  height: 100%;
+}
+
+.vfx-keyvision-ball_canvas {
   width: 100%;
   height: 100%;
   left: 0;
