f4c87bb9ff1e3392f9823d0ad79e6a8f801e51a0
vivi <viboloveyou12@gmail.com>
2019-12-15 13:27:49 +0800

    製作activity內容

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -8,12 +8,21 @@
             <div class="activity-mainTop-left">
                 <h1>清香純純</h1>
                 <h2>手繪1930年代台灣第一女歌手</h2>
-                <div class="activity-date_mobile"></div>
-                <div class="activity-title-detail">
-                    <div class="activity-detail-time"></div>
+                <div class="activity-detail_mobile"></div>
+                <div class="activity-detail-desktop">
+                    <div class="activity-detail-time">
+                      <img src="@/assets/img/activities/dot.png">16:00-17:30
+                    </div>
+                    <div class="activity-detail-place">
+                      <img src="@/assets/img/activities/dot.png">剝皮寮歷史街區 13號展間
+                    </div>
+                    <div class="activity-detail-speaker">
+                      <img src="@/assets/img/activities/dot.png">老師_黃郁婷/手繪漫畫師
+                    </div>
                 </div>
             </div>
             <div class="activity-date_desktop">
+              <img src="@/assets/img/activities/bigdate18.png">
             </div>
         </div>
         <div class="activity-mainBottom">
@@ -22,10 +31,24 @@
                 讓大家學習如何調色與配色，也讓大家可以動手DIY。
             </p>
             <div class="activity-mainBottom-picture">
-                <div class="mainBottom-picture_desktop"></div>
+                <div class="mainBottom-picture_desktop">
+                  <div class="picture">
+                    <img src="@/assets/img/activities/activity/speaker1/speak1_s3.jpg">
+                    介紹收藏
+                  </div>
+                  <div class="picture">
+                    <img src="@/assets/img/activities/activity/speaker1/speak1_s3.jpg">
+                    介紹收藏
+                  </div>
+                </div>
                 <div class="mainBottom-picture_mobile"></div>
             </div>
         </div>
+    </div>
+    <div class="bigpicture_desktop">
+      <div class="bigpicture-background"></div>
+      <img class="cross" src="@/assets/img/activities/activity/cross.png">
+      <img src="@/assets/img/activities/activity/speaker1/speak1_l3.jpg">
     </div>
     <div class="activity-goBtn"></div>
   </div>
