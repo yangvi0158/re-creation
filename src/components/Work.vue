52acd21d45f1f602d51ce183227cef6186bda54e
vivi <viboloveyou12@gmail.com>
2019-12-17 11:52:22 +0800

    串接projects資料

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -5,24 +5,19 @@
         <button>Back</button>
       </div>
       <div class="workMain">
-        <h1>春風弦雨 - </h1>
-        <h2>鄧雨賢非虛構圖文轉化</h2>
-        <p>
-        這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介，
-        這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介，
-        這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介，
-        這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介，
-        這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介，
-        這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介這是展覽簡介，
-        </p>
+        <h1>{{ workData[0].title }}</h1>
+        <h2>{{ workData[0].subtitle }}</h2>
+        <p>{{ workData[0].projectInfo }}</p>
         <div class="workAuthor">
-          <div class="author">黃郁婷</div>
-          <div class="author">黃郁婷</div>
+          <div class="author" v-for="item in workData[0].author" :key="item.name">
+            <img :src="item.img">
+            {{item.name}}
+          </div>
         </div>
       </div>
     </div>
     <div class="workRight">
-      <img src="@/assets/img/test.jpg">
+      <img :src="workData[0].photo[0].imgUrl">
     </div>
   </div>
 </template>
@@ -31,28 +26,43 @@ import axios from 'axios'
 export default {
   name: 'work',
   props: {
-    msg: String
+    workId: String,
+    workNum: Number
   },
   data () {
     return {
-      workData: []
+      workid: this.workId,
+      worknum: this.workNum,
+      projectsData: [],
+      workData: {}
     }
   },
   methods: {
     closeProject () {
       this.$emit('close-project')
-      console.log('close!')
+    },
+    getWorkData () {
+      let vm = this
+      this.workData = vm.projectsData.filter((item) => item.id === vm.workid)
+      console.log(this.workData)
     }
   },
   created () {
     let vm = this
     axios.get('js/projects.json').then(res => {
-      vm.lworkData = res.data
-      console.log(vm.workData)
-      console.log('vm.workData')
+      vm.projectsData = res.data[vm.worknum]
+      vm.getWorkData()
     }).catch(err => {
       console.log(err, '失敗')
     })
+  },
+  watch: {
+    workId () {
+      this.workid = this.workId
+    },
+    workNum () {
+      this.worknum = this.workNum
+    }
   }
 }
 </script>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -5,11 +5,9 @@
       <h1>屮屮之間，</h1>
       <p>這片土地上的大小事，這片土地「艸」網羅了千千萬萬個故事。</p>
       <ul class="project-team">
-        <button @click="openProject">OPEN</button>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li v-for="item in projectsData[0]" :key="item.id">
+          <img :src="item.logo" @click="openProject(0,item.id)">
+        </li>
       </ul>
     </div>
 
@@ -18,11 +16,9 @@
       <h1>日日之間，</h1>
       <p>生活日常的故事，這些有趣的故事彙集成「昍」，如同滿片星星般光明閃亮。</p>
       <ul class="project-team">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li v-for="item in projectsData[1]" :key="item.id">
+          <img :src="item.logo" @click="openProject(1,item.id)">
+        </li>
       </ul>
     </div>
 
@@ -31,27 +27,23 @@
       <h1>手手之間，</h1>
       <p>在每雙巧手之間，打造出的工藝故事，兩手之間「抙」引聚匯流了這些工藝故事。</p>
       <ul class="project-team">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li v-for="item in projectsData[2]" :key="item.id">
+          <img :src="item.logo" @click="openProject(2,item.id)">
+        </li>
       </ul>
     </div>
 
-    <div class="project-category">
+    <div class="project-category last-category">
       <h6>04</h6>
       <h1>人人之間，</h1>
       <p>人與人之間的故事，這些故事的交集成了「从」，象徵著跟隨前人的引導前進。</p>
       <ul class="project-team">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li v-for="item in projectsData[3]" :key="item.id">
+          <img :src="item.logo" @click="openProject(3,item.id)">
+        </li>
       </ul>
     </div>
-    <Work v-show="showProject" @close-project="showProject = false"/>
+    <Work :work-id="workid" :work-num="worknum" v-if="showProject" @close-project="showProject = false"/>
   </div>
 </template>
 
@@ -65,17 +57,24 @@ export default {
   },
   data () {
     return {
-      showProject: false
+      showProject: false,
+      projectsData: [],
+      workid: '',
+      worknum: 0
     }
   },
   methods: {
-    openProject () {
+    openProject (num, id) {
       this.showProject = true
+      this.workid = id
+      this.worknum = num
     }
   },
   mounted () {
+    let vm = this
     axios.get('js/projects.json').then(res => {
-      console.log(res.data)
+      vm.projectsData = res.data
+      console.log('projectsData', vm.projectsData)
     }).catch(err => {
       console.log(err, '失敗')
     })
